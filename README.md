20d5bd62a0aaa5fcab4d0aae1a168c4ce266baad
AlexRToledo <artoledo91@gmail.com>
2019-09-01 23:58:28 -0500

    [ADD, MDF] Webpack config and ready for start.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# runic-css
+# RUNIC-CSS Framework
+1.  Clone the project
+2.  Move to project folder
+3.  Into a console make: `npm install`
+4.  Aviable commands:
+    1. `npm build` - Build library into dist folder ready to use.
+    2. `npm compile` - Build library into dist folder not minified.
+    2. `npm start` - Launch framework documentation.
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,51 @@
 
-const path = require('path');
+const path = require('path'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    { CleanWebpackPlugin } = require('clean-webpack-plugin'),
+    MiniCssExtractPlugin = require('mini-css-extract-plugin'),
+    OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-    mode: 'none',
-    entry: './src/entries/index.js',
+    mode: process.env.WEBPACK_MODE,
+    entry: ['./src/entries/index.js', './src/sass/main.scss'],
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    optimization: {
+        minimizer: [new OptimizeCSSAssetsPlugin({})],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({        
+            filename: 'css/style.css',
+            chunkFilename: '[id].css',
+            ignoreOrder: false,
+          }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'docs/index.pug'
+        })
+    ],
+    module: {
+        rules: [
+            {
+              test: /\.s[ac]ss$/i,
+              use: [
+                {
+                    loader: MiniCssExtractPlugin.loader
+                },
+                'css-loader',
+                'sass-loader',
+              ],
+            },
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader'
+            }
+          ]
+    },
     output: {
-        filename: 'main.js',
+        filename: 'js/main.js',
         path: path.resolve(__dirname, 'dist')
     }    
 };
